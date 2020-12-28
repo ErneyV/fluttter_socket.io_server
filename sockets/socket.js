67a5ce76349c825f9b4c845236c9d3d5c8ca8f9b
Erney Vargas <erney.vargas@MCT.COM.CO>
2020-12-27 23:17:57 -0500

    Fin seccion 3

---
+++ sockets/socket.js
@@ -0,0 +1,15 @@
+const { io } = require('../index');
+
+io.on('connection', client => {
+    console.log('Cliente Conectado');
+
+    client.on('disconnect', () => {
+        console.log('Cliente desconectado');
+    });
+
+    client.on('mensaje', (payload) => {
+        console.log('Mensaje', payload);
+
+        io.emit('mensaje', { admin: 'nuevo Mensaje del Administrador' })
+    })
+});
